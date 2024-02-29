5c3fc1afc76035c13529b678203ebc2521b5bc18
adi gunawan <adi_gunawan@live.com>
2024-02-29 12:17:27 +0800

    install wifi module and current time

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,40 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
-
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
 #define DHTTYPE    DHT11     // DHT 22 (AM2302)
 
 DHT_Unified dht(D4, DHTTYPE);
 
 uint32_t delayMS;
 
-void setup()  
- {  
-  Serial.begin(115200);
+const char* ssid = "Rumah Koming";
+const char* password = "24072007";
 
+const long utcOffsetInSeconds = 8 * 3600; // UTC+8 offset in seconds
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+
+
+void setup()  
+ { 
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println("Connected!");
+
+  // Initialize time client
+  timeClient.begin();
+
+  Serial.begin(115200);
+  pinMode(D0, OUTPUT);
     // Initialize device.
   dht.begin();
   Serial.println(F("DHTxx Unified Sensor Example"));
@@ -40,6 +63,14 @@ void setup()
   Serial.println(F("------------------------------------"));
   // Set delay between sensor readings based on sensor details.
   delayMS = sensor.min_delay / 1000;
+  digitalWrite(D0, HIGH);
+
+  // Update time
+  timeClient.update();
+
+  // Print formatted time
+  Serial.print("Current time: ");
+  Serial.println(timeClient.getFormattedTime());
 }  
 
 void loop() {  
