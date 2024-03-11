f62aa7d1ce6924f67151226b465234c953e8a636
adi gunawan <adi_gunawan@live.com>
2024-03-11 11:09:37 +0800

    update tag

--- src/config.h
+++ src/config.h
@@ -13,5 +13,5 @@ extern const unsigned long DELAY_MS;
 
 #define RELEASE_URL "https://github.com/zgunz42/servercopilot-iot/releases/latest"
 #define DELAY_MS 1000
-#define RELEASE_VERSION "0.0.1"
+#define RELEASE_VERSION "v0.0.1"
 #endif
